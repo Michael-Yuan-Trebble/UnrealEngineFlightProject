7b2ecc4ca460d9cb7afbac2f63cfc1c9f30417e0
Michael-Yuan-Trebble <michaelwmyuan@gmail.com>
2025-09-18 22:10:41 -0400

    Reset repo to sync with folder hierarchy

--- Source/MyProject2/Private/CurrentPlayerState.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "CurrentPlayerState.h"
-#include "R60.h"
-
-ACurrentPlayerState::ACurrentPlayerState() {
-	SelectedWeapons.Add(AR60::StaticClass());
-}
